5636574d76ba56a04922093ec65c0e5f8fd62f2a
Adamki <adajensen@gmail.com>
2016-11-23 01:12:05 -0800

    taskList and taskRow implemented with Navigator spike

--- main.js
+++ main.js
@@ -1,28 +1,74 @@
 import Exponent from 'exponent';
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
   View,
+  StyleSheet,
+  Navigator,
+  Text,
 } from 'react-native';
 
-class App extends React.Component {
+import TaskList from './taskList';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+  }
+})
+
+class PsToDo extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      todos: [
+        {
+          task: "Learn React Native"
+        },
+        {
+          task: "Get a Job"
+        },
+        {
+          task: "Go do laundry"
+        },
+      ],
+    };
+  }
+
+  onAddStarted() {
+    this.nav.push({
+      name: 'taskform',
+    })
+  }
+
+  renderScene(route, nav) {
+    switch(route.name) {
+      case 'taskform':
+        return <Text>Add form comes here</Text>
+      default:
+        return (
+          <TaskList
+            todos={this.state.todos}
+            onAddStarted={this.onAddStarted.bind(this)}
+          />
+        );
+      }
+  }
+
+  configureScene() {
+    return Navigator.SceneConfigs.FloatFromBottom;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
+      <Navigator
+        configureScene={this.configureScene}
+        initialRoute={{name: 'tasklist', index: 0}}
+        renderScene={this.renderScene.bind(this)}
+        ref={(nav) => {
+          this.nav = nav
+        }}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-Exponent.registerRootComponent(App);
+Exponent.registerRootComponent(PsToDo);
